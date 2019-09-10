190f6028db5301ee7795ae4c5c7732247d8efc17
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2019-09-10 18:56:11 +0200

    Add QuestionForm

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {FaQuestionCircle} from 'react-icons/fa';
+
+import {Card, Input, Button} from './ui';
+
+import {addQuestion} from '../actions/questions';
+
+import './QuestionForm.scss';
+
+class QuestionForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      optionOneText: '',
+      optionTwoText: '',
+    };
+  }
+
+  handleChange = evt => this.setState({[evt.target.name]: evt.target.value});
+
+  addAndClean = evt => {
+    evt.preventDefault();
+    const {dispatch, authUser} = this.props;
+    const {optionOneText, optionTwoText} = this.state;
+    if (optionOneText && optionTwoText) {
+      dispatch(
+        addQuestion({optionOneText, optionTwoText, author: authUser.id}),
+      ).then(() => {
+        this.setState({ optionOneText: '', optionTwoText: '' });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="question-form-container">
+        <Card title="Would you rather...">
+          <form onSubmit={this.addAndClean}>
+            <div className="question-layout">
+              <div className="input-holder">
+                <Input
+                  onChange={this.handleChange}
+                  name="optionOneText"
+                  value={this.state.optionOneText}
+                />
+                <br />
+                OR
+                <br />
+                <Input
+                  onChange={this.handleChange}
+                  name="optionTwoText"
+                  value={this.state.optionTwoText}
+                />
+                <br />
+              </div>
+              <i>
+                <FaQuestionCircle size={40} color="#1181C8" />
+              </i>
+            </div>
+            <br />
+            <Button type="submit" color="secondary">
+              Save
+            </Button>
+          </form>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  authUser: state.users.authUser,
+});
+
+export default connect(mapStateToProps)(QuestionForm);
